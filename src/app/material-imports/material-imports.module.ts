3d1f2aac00b0874482a85d43651d986d2ab74512
nurul69 <nurulusmani69@gmail.com>
2021-11-06 00:36:01 +0530

    Upstream fix

--- src/app/material-imports/material-imports.module.ts
+++ src/app/material-imports/material-imports.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 @NgModule({
@@ -9,7 +11,9 @@ import {MatButtonModule} from '@angular/material/button';
     CommonModule
   ],
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule
   ]
 })
 export class MaterialImportsModule { }
