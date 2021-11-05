218b755f1864f6277da475ab56aa67f0b94b8c01
nurul69 <nurulusmani69@gmail.com>
2021-11-05 23:43:26 +0530

    Intial component development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialImportsModule } from './material-imports/material-imports.module';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingsComponent } from './trainings/trainings.component';
+import { NewTrainingComponent } from './trainings/new-training/new-training.component';
+import { CurrentTrainingComponent } from './trainings/current-training/current-training.component';
+import { CompletedTrainingComponent } from './trainings/completed-training/completed-training.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingsComponent,
+    NewTrainingComponent,
+    CurrentTrainingComponent,
+    CompletedTrainingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialImportsModule
   ],
-  providers: [],
+  providers: [MaterialImportsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/trainings/completed-training/completed-training.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CompletedTrainingComponent } from './completed-training.component';
+
+describe('CompletedTrainingComponent', () => {
+  let component: CompletedTrainingComponent;
+  let fixture: ComponentFixture<CompletedTrainingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CompletedTrainingComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CompletedTrainingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
